c12263b1607f5fe314dd6e262348e2f8188a982b
avanalphen <vanalphena@gmail.com>
2021-07-22 15:42:56 -0400

    Merge branch 'main' of https://github.com/Juncie/WriterX

--- backend/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-router.post('/add-post', authorize, async (req, res) => {
-
-    let newPost = req.body
-    newPost.userId = res.locals.user._id
-    console.log(res)
-    Post.create(newPost).then(post => {
-        res.json(post)
-    })
-})
